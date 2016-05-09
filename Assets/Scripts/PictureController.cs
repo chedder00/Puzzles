22f8d1141b347a3a3abe9c413c9bc64fffee76a1
Chad Greene <Chad Greene>
2016-05-08 17:21:39 -0700

    Added in Puzzle 6

---
+++ Assets/Scripts/PictureController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class PictureController : MonoBehaviour {
+
+    WebCamTexture _webcam;
+    public Image _window;
+
+	// Use this for initialization
+	void Start () {
+        _webcam = new WebCamTexture();
+        _window.material.mainTexture = _webcam;
+        _webcam.Play();        
+	}
+
+    public void TakePhoto()
+    {
+        StartCoroutine(CapturePicture());
+    }
+
+    IEnumerator CapturePicture()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D photo = new Texture2D(_webcam.width, _webcam.height);
+        photo.SetPixels(_webcam.GetPixels());
+        photo.Apply();
+
+        //Encode to a PNG
+        byte[] bytes = photo.EncodeToPNG();
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes("photo.png", bytes);
+    }
+}
